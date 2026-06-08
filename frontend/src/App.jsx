fe7e7108f610709fd1e3c3d835ce524d48809705
singhutp-glitch <singhutp@gmail.com>
2026-06-08 21:18:10 +0530

    Make sidebar with click open

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import SideBar from "./components/sideBar/SideBar"
+import SideBar from "./components/SideBar/SideBar"
 
 
 function App() {
--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SideBar.css'
 import {assets} from '../../assets/assets.js'
 const SideBar = () => {
+  const [extended,setExtended] = useState(false);
+
+
   return (
     <div className='side-bar'>
       <div className='top'>
-        <img className="menu" src={assets.menu_icon} alt="" />
+        <img onClick={()=>{setExtended(prev=>!prev)}} className="menu" src={assets.menu_icon} alt="" />
       <div className='new-chat'>
         <img src={assets.plus_icon} alt="" />
-        <p>New Chat</p>
+        {extended?<p>New Chat</p>:null}
       </div>
+      {extended?
       <div className="recent">
         <p className='recent-title'>Recent</p>
         <div className="recent-entry">
@@ -17,23 +21,24 @@ const SideBar = () => {
           <p>what is React ...</p>
         </div>
       </div>
+        :null}
       </div>
       <div className='bottom'>
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended?<p>Help</p>:null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended?<p>Activity</p>:null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Setting</p>
+          {extended?<p>Setting</p>:null}
         </div>
       </div>
     </div>
   )
 }
 
-export default sideBar
+export default SideBar
