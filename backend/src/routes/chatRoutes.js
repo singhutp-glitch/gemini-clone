7c9bcfd20fa121c57d20af2b4b5f63c65ce6169c
singhutp-glitch <singhutp@gmail.com>
2026-06-22 19:36:33 +0530

    Add authorization to routes

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router();
 import controller from '../controllers/chatController.js'
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
-router.get('/', controller.loadChatsGet);
-router.post('/',controller.createChatPost);
+router.get('/',authMiddleware, controller.loadChatsGet);
+router.post('/',authMiddleware,controller.createChatPost);
 
-router.get('/:chatId/messages', controller.loadMessagesGet);
-router.post('/:chatId/messages', controller.sendMessage);
+router.get('/:chatId/messages',authMiddleware, controller.loadMessagesGet);
+router.post('/:chatId/messages', authMiddleware,controller.sendMessage);
 
 
 export default router;
