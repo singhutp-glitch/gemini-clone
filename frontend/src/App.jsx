31eafe753d25171140e312239bb70ece4c1376b9
singhutp-glitch <singhutp@gmail.com>
2026-06-22 11:55:48 +0530

    Add register component to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,18 @@ import ChatGraph from "./components/ChatGraph/ChatGraph";
 import { useState ,useEffect} from "react"
 import { getChats } from "./services/api";
 import ChatPage from "./components/ChatPage/ChatPage";
+import Register from './components/RegisterPage/RegisterPage'
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [authMode, setAuthMode] = useState("register");
 
   return (
     <>
-      <ChatPage/>
+    {user?<ChatPage/>:
+    authMode==='register'?<Register/>:
+    ''}
+    
     </>
   )
 }
