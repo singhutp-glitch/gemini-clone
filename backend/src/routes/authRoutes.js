3ee2994a3d951c68f156af9c5fdc00f4fc9cf894
singhutp-glitch <singhutp@gmail.com>
2026-06-22 14:43:42 +0530

    Add login route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,5 +5,6 @@ import authValidator from '../validators/authValidator.js'
 const router = express.Router();
 
 router.post("/register", authValidator,authController.register);
+router.post("/login", authValidator,authController.login);
 
 export default router;
