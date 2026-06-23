f6add32124d562cd99f4e679418b5ed17838d9c9
singhutp-glitch <singhutp@gmail.com>
2026-06-23 21:27:21 +0530

    Make folders for search workflow

--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -1,16 +1,13 @@
 import { tavily } from "@tavily/core";
-import 'dotenv/config';
+//import 'dotenv/config';
 
 const tvly = tavily({ apiKey:process.env.TAVILY_API_KEY });
 
-async function webSearch(query){
+export async function searchWeb(query){
     try{
         const response = await tvly.search(query);
         return response;
     }catch(error){
         console.error(error);
     }
-}
-const result = await webSearch('who is leaoni messi');
-console.log(result);
-
+};
